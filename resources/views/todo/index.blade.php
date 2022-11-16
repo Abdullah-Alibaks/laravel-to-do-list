31972e06b639675c21575de526498cb31605aa1c
Abdullah Alibaks <abdullah.alibaks@student.rocva.nl>
2022-11-16 12:36:41 +0100

    1.0

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,46 @@
+<?php /* @var \App\Models\Todo[] $todos */ ?>
+
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <ul class="todo-list">
+            @foreach($todos as $todo)
+                <li class="todo-item">
+                    <form class="todo-item__form" action="{{route('todos.update', $todo)}}" method="post">
+                        @method('PUT')
+                        @csrf
+                        <input type="checkbox" name="done" {{ $todo->done ? 'checked' : null }}>
+                    </form>
+                    <div class="todo-item__content">
+                        <h3>{{ ucfirst($todo->title) }}</h3>
+                        <p>{{ $todo->content }}</p>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+
+        <h2>Create To-do</h2>
+
+        <form action="{{route('todos.store')}}" method="post" class="create-todo">
+            @csrf
+            <div class="create-todo__input-group">
+                <label for="title">Title</label>
+                <input id="title" type="text" name="title">
+            </div>
+            <div class="create-todo__input-group">
+                <label for="task-add">Koppel task aan:</label>
+                <select name="todos" id="todos">
+                    <option disable selected>--selecteer task--</option>
+                    @foreach($todos as $todo)
+                    <option value="{{$todo -> id}}">{{$todo -> title}}</option>
+                    @endforeach
+            </div>
+            <div class="create-todo__input-group">
+                <label for="content">Description</label>
+                <textarea id="content" name="content"cols="30" rows="10"></textarea>
+            </div>
+            <button class="button" type="submit">Save</button>
+        </form>
+    </div>
+@endsection
